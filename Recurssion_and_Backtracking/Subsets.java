f462d500ebaeeb6001556eaed7e16df64a418e4a
mukul-457 <mukulacharya.1911@gmail.com>
2024-07-05 01:02:38 +0530

    Easy . Subset problem with unique numbers

---
+++ Recurssion_and_Backtracking/Subsets.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets {
+    public static void main(String[] args) {
+        Solution s  = new Solution();
+        System.out.println(s.subsets(new int[]{1,2,3}));
+    }
+}
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> solutions = new ArrayList<>();
+        int n = nums.length;
+        List<Integer> currSubset = new ArrayList<>();
+        createSubset(nums, currSubset, 0,solutions);
+        return solutions;
+    }
+
+    public void createSubset(int[] nums , List<Integer> currSubset, int index, List<List<Integer>> solutions){
+        if (index == nums.length){
+            solutions.add(new ArrayList<>(currSubset));
+            return;
+        }
+        currSubset.add(nums[index]);
+        createSubset(nums , currSubset, index+1,solutions);
+        currSubset.remove(currSubset.size()-1);
+        createSubset(nums, currSubset, index+1, solutions);
+
+    }
+}
